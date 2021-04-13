0e94c6237410514035dfbf75dbea40d62472f0bf
Shteryo16 <47269119+Shteryo16@users.noreply.github.com>
2021-04-13 18:56:23 +0300

    Added PaymentService

--- Land_Manager/Services/LandService.cs
+++ Land_Manager/Services/LandService.cs
@@ -7,8 +7,36 @@ using System.Linq;
 namespace Services
 {
     
-    public class PropertyService
+    public class LandService :ILand
     {
-        
+        private RmsContext _context;
+        public LandService(RmsContext context)
+        {
+            _context = context;
+        }
+        public void Add(Land property)
+        {
+            _context.Add(property);
+            _context.SaveChanges();
+        }
+        public Land Get(int id)
+        {
+            return GetAll().FirstOrDefault(p => p.Id == id);
+        }
+        public IEnumerable<Land> GetAll()
+        {
+            return _context.Lands.Include(p => p.Customers);
+        }
+        public int GetNumberOfLands()
+        {
+            return _context.Lands.Count();
+        }
+        public void Update(Land property)
+        {
+            Land entityToUpdate = Get(property.Id);
+            _context.Entry(entityToUpdate).CurrentValues.SetValues(property);
+
+            _context.SaveChanges();
+        }
     }
 }
