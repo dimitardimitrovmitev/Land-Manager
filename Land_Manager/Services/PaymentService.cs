c0cb15bdfb729f2a0a6a0380dca707c1ab20757f
Shteryo16 <47269119+Shteryo16@users.noreply.github.com>
2021-04-13 18:59:07 +0300

    Added PaymentService

--- Land_Manager/Services/PaymentService.cs
+++ Land_Manager/Services/PaymentService.cs
@@ -6,8 +6,89 @@ using System.Linq;
 
 namespace Services
 {
-    public class PaymentService
+    public class PaymentService : IPayment
     {
-        
+        private RmsContext _context;
+        public PaymentService(RmsContext context)
+        {
+            _context = context;
+        }
+        public void Add(Payment payment)
+        {
+            _context.Add(payment);
+            _context.SaveChanges();
+        }
+        public Payment Get(int id)
+        {
+            return GetAll().FirstOrDefault(p => p.Id == id);
+        }
+
+
+        public IEnumerable<Payment> GetAll()
+        {
+            return _context.Payments.Include(p => p.Customer).OrderByDescending(p => p.Date);
+        }
+
+
+        public IEnumerable<Payment> GetAllFromCustomer(int customerId)
+        {
+            return GetAll().Where(p => p.Customer.Id == customerId);
+        }
+
+
+        public double GetPaymentSum(int customerId)
+        {
+            double sum = 0;
+
+            foreach (var payment in GetAllFromCustomer(customerId))
+            {
+                sum += payment.Amount;
+            }
+
+            return sum;
+        }
+
+
+        public IEnumerable<Payment> GetAllFromMonth(int month)
+        {
+            return GetAll().Where(p => p.Date.Month == month);
+        }
+
+
+        public double GetAmountFromMonth(int month)
+        {
+            IEnumerable<Payment> payments = GetAllFromMonth(month);
+
+            return GetPaymentsAmount(payments);
+        }
+
+
+        public double GetAmountFromYear(int year)
+        {
+            IEnumerable<Payment> payments = GetAll().Where(p => p.Date.Year == year);
+
+            return GetPaymentsAmount(payments);
+        }
+
+
+        public double GetAmountFromAllTime()
+        {
+            IEnumerable<Payment> payments = GetAll();
+
+            return GetPaymentsAmount(payments);
+        }
+
+
+        private double GetPaymentsAmount(IEnumerable<Payment> payments)
+        {
+            double sum = 0;
+
+            foreach (var payment in payments)
+            {
+                sum += payment.Amount;
+            }
+
+            return sum;
+        }
     }
 }
