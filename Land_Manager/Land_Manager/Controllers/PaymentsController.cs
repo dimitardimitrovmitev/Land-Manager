2f1dc3d6590b63fde3c003fb4705f7ebc4e44576
Shteryo16 <47269119+Shteryo16@users.noreply.github.com>
2021-04-14 18:18:47 +0300

    Added PaymentsController

--- Land_Manager/Land_Manager/Controllers/PaymentsController.cs
+++ Land_Manager/Land_Manager/Controllers/PaymentsController.cs
@@ -7,6 +7,57 @@ namespace Land_Manager.Controllers
 {
     public class PaymentsController : Controller
     {
-        
+        private readonly IPayment _payments;
+        private readonly ICustomer _customers;
+
+        public PaymentsController(IPayment payments, ICustomer customers)
+        {
+            _payments = payments;
+            _customers = customers;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult All()
+        {
+            PaymentListModel model = new PaymentListModel()
+            {
+                Payments = _payments.GetAll().Select(c => new PaymentItemModel()
+                {
+                    CustomerId = c.Customer.Id,
+                    CustomerName = c.Customer.FullName,
+                    Cost = c.Amount,
+                    Date = c.Date.ToString("dd/MM/yyyy")
+                })
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Customer(int id)
+        {
+            if (!_customers.HasPayments(id))
+            {
+                return RedirectToAction("All");
+            }
+
+            PaymentsFromCustomerListModel model = new PaymentsFromCustomerListModel()
+            {
+                Payments = _payments.GetAllFromCustomer(id).Select(t => new PaymentItemModel()
+                {
+                    CustomerId = t.Customer.Id,
+                    CustomerName = t.Customer.FullName,
+                    Cost = t.Amount,
+                    Date = t.Date.ToString("dd/MM/yyyy")
+                }),
+                CustomerId = id,
+                CustomerName = _customers.Get(id).FullName
+            };
+
+            return View(model);
+        }
     }
 }
