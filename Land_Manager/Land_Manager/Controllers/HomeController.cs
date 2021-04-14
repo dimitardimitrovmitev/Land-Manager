e63948f22c7a9da9c60b0294d91c52a37d415aa7
dimitardimitrovmitev <47269111+dimitardimitrovmitev@users.noreply.github.com>
2021-04-14 18:30:24 +0300

    Added HomeController

--- Land_Manager/Land_Manager/Controllers/HomeController.cs
+++ Land_Manager/Land_Manager/Controllers/HomeController.cs
@@ -10,6 +10,41 @@ namespace Land_Manager.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private readonly ILand _lands;
+        private readonly ICustomer _customers;
+        private readonly IPayment _payments;
+
+        public HomeController(ILand properties, ICustomer customers, IPayment payments)
+        {
+            _lands = properties;
+            _customers = customers;
+            _payments = payments;
+        }
+
+        public IActionResult Index()
+        {
+            int month = DateTime.Now.Month;
+            HomeViewModel model = new HomeViewModel
+            {
+                LandCount = _lands.GetNumberOfLands(),
+                CustomerCount = _customers.GetNumberOfCustomers(),
+                RentCollectedThisMonth = _payments.GetAmountFromMonth(month),
+                PaymentsThisMonth = _payments.GetAllFromMonth(month).Select(p => new PaymentItemModel()
+                {
+                    CustomerId = p.Customer.Id,
+                    CustomerName = p.Customer.FullName,
+                    Cost = p.Amount,
+                    Date = p.Date.ToString("dd/MM/yyyy")
+                })
+            };
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
