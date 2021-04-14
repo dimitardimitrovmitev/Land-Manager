28c2ef716dbaceb2f69d2727312a8f3891855aa4
Shteryo16 <47269119+Shteryo16@users.noreply.github.com>
2021-04-14 18:21:19 +0300

    Added RentCollectedController

--- Land_Manager/Land_Manager/Controllers/RentCollectedController.cs
+++ Land_Manager/Land_Manager/Controllers/RentCollectedController.cs
@@ -7,6 +7,26 @@ namespace Land_Manager.Controllers
 {
     public class RentCollectedController : Controller
     {
-        
+        private readonly IPayment _payments;
+
+        public RentCollectedController(IPayment payments)
+        {
+            _payments = payments;
+        }
+
+        public IActionResult Index()
+        {
+            int month = DateTime.Now.Month;
+            int year = DateTime.Now.Year;
+
+            RentCollectedIndexModel model = new RentCollectedIndexModel()
+            {
+                MonthlyProfits = _payments.GetAmountFromMonth(month),
+                YearlyProfits = _payments.GetAmountFromYear(year),
+                AllTimeProfits = _payments.GetAmountFromAllTime()
+            };
+
+            return View(model);
+        }
     }
 }
