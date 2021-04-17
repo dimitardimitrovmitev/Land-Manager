bdb109987ba1f0e24d8a79761b7ddfc7b5d3ec5f
Shteryo16 <47269119+Shteryo16@users.noreply.github.com>
2021-04-17 12:54:34 +0300

    Added documentation for Services

--- Land_Manager/Services/LandService.cs
+++ Land_Manager/Services/LandService.cs
@@ -6,31 +6,58 @@ using System.Linq;
 
 namespace Services
 {
-    
+    /// <summary>
+    /// Handles the land business logic
+    /// </summary>
     public class LandService :ILand
     {
         private RmsContext _context;
+        /// <summary>
+        /// Constuctor for the Land Service
+        /// </summary>
+        /// <param name="context">Database context</param>
         public LandService(RmsContext context)
         {
             _context = context;
         }
+        /// <summary>
+        /// Adds a new land to the Database
+        /// </summary>
+        /// <param name="land"></param>
         public void Add(Land land)
         {
             _context.Add(land);
             _context.SaveChanges();
         }
+        /// <summary>
+        /// Returns a single land, specified by id
+        /// </summary>
+        /// <param name="id">A land matching the id or null if none matches</param>
+        /// <returns></returns>
         public Land Get(int id)
         {
             return GetAll().FirstOrDefault(p => p.Id == id);
         }
+        /// <summary>
+        /// Returns all land records
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Land> GetAll()
         {
             return _context.Lands.Include(p => p.Customers);
         }
+        /// <summary>
+        /// Gets the number of all lands
+        /// </summary>
+        /// <returns></returns>
         public int GetNumberOfLands()
         {
             return _context.Lands.Count();
         }
+        /// <summary>
+        /// Update the values of a single land
+        /// </summary>
+        /// <param name="land"></param>
         public void Update(Land land)
         {
             Land entityToUpdate = Get(land.Id);
--- Land_Manager/Services/PaymentService.cs
+++ Land_Manager/Services/PaymentService.cs
@@ -6,88 +6,119 @@ using System.Linq;
 
 namespace Services
 {
+    /// <summary>
+    /// Handles the payment business logic
+    /// </summary>
     public class PaymentService : IPayment
     {
         private RmsContext _context;
+        /// <summary>
+        /// Constuctor for the Payment Service
+        /// </summary>
+        /// <param name="context"></param>
         public PaymentService(RmsContext context)
         {
             _context = context;
         }
+        /// <summary>
+        /// Adds a new Payment to the Database
+        /// </summary>
+        /// <param name="payment"></param>
         public void Add(Payment payment)
         {
             _context.Add(payment);
             _context.SaveChanges();
         }
+        /// <summary>
+        /// Returns a single payment, specified by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Payment Get(int id)
         {
             return GetAll().FirstOrDefault(p => p.Id == id);
         }
-
-
+        /// <summary>
+        /// Returns all payment records ordered by date in descending order
+        /// </summary>
+        /// <returns>All the Payments</returns>
         public IEnumerable<Payment> GetAll()
         {
             return _context.Payments.Include(p => p.Customer).OrderByDescending(p => p.Date);
         }
-
-
+        /// <summary>
+        /// All payments from a specific customer
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
         public IEnumerable<Payment> GetAllFromCustomer(int customerId)
         {
             return GetAll().Where(p => p.Customer.Id == customerId);
         }
-
-
+        /// <summary>
+        /// Get the sum of all payments made by a specific customer
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
         public double GetPaymentSum(int customerId)
         {
             double sum = 0;
-
             foreach (var payment in GetAllFromCustomer(customerId))
             {
                 sum += payment.Amount;
             }
-
             return sum;
         }
-
-
+        /// <summary>
+        /// Get all payments made on a specific month
+        /// </summary>
+        /// <param name="month"></param>
+        /// <returns></returns>
         public IEnumerable<Payment> GetAllFromMonth(int month)
         {
             return GetAll().Where(p => p.Date.Month == month);
         }
-
-
+        /// <summary>
+        /// Get sum of payments made on a specific month
+        /// </summary>
+        /// <param name="month"></param>
+        /// <returns></returns>
         public double GetAmountFromMonth(int month)
         {
             IEnumerable<Payment> payments = GetAllFromMonth(month);
-
             return GetPaymentsAmount(payments);
         }
-
-
+        /// <summary>
+        /// Get sum of payments made from a specific year
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
         public double GetAmountFromYear(int year)
         {
             IEnumerable<Payment> payments = GetAll().Where(p => p.Date.Year == year);
-
             return GetPaymentsAmount(payments);
         }
-
-
+        /// <summary>
+        /// Get the sum of all payments regardless of the date
+        /// </summary>
+        /// <returns></returns>
         public double GetAmountFromAllTime()
         {
             IEnumerable<Payment> payments = GetAll();
-
             return GetPaymentsAmount(payments);
         }
-
-
+        /// <summary>
+        /// Get the sum of money from a list of payments
+        /// </summary>
+        /// <param name="payments"></param>
+        /// <returns></returns>
         private double GetPaymentsAmount(IEnumerable<Payment> payments)
         {
             double sum = 0;
-
             foreach (var payment in payments)
             {
                 sum += payment.Amount;
             }
-
             return sum;
         }
     }
